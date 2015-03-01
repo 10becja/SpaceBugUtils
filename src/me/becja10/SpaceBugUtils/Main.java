5b76486f9b072a615312fb212f0b2b0891fff939
James <10becja@gmail.com>
2015-02-28 19:43:05 -0600

    Where to start....
    +Track who kills ender dragon
    +Prevent crafting special banners. can only craft basic or copy
    +improved preventing players from going above nether
    +get list of longest players online

--- src/me/becja10/SpaceBugUtils/Main.java
+++ src/me/becja10/SpaceBugUtils/Main.java
@@ -1,33 +1,63 @@
 package me.becja10.SpaceBugUtils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+
+import me.becja10.SpaceBugUtils.FileManager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener
 {
 	public final Logger logger = Logger.getLogger("Minecraft");
+	private static Main plugin;
+	private Player lastAttacker;
+	private Entity dragon;
+
+	private Map<String, Long> joined = new HashMap<String, Long>();
+	private List<String> list = new LinkedList<String>();
 
 	public void onEnable()
 	{
 		PluginDescriptionFile pdfFile = getDescription();
 		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
+		plugin = this;
+	    FileManager.saveDefaultPlayers();
+
 	}
 
 	public void onDisable()
@@ -36,16 +66,62 @@ public class Main extends JavaPlugin implements Listener
 		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
 	}
 	
+	@EventHandler(priority=EventPriority.LOWEST)
+	public void onJoin(PlayerJoinEvent event)
+	{
+		joined.put(event.getPlayer().getDisplayName(), System.currentTimeMillis());
+		list.add(event.getPlayer().getDisplayName());
+	}
+	
+	@EventHandler(priority=EventPriority.LOWEST)
+	public void onQuite(PlayerQuitEvent event)
+	{
+		joined.remove(event.getPlayer().getDisplayName());
+		list.remove(event.getPlayer().getDisplayName());
+	}
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
+		//onlinelongest
+		if(cmd.getName().equalsIgnoreCase("onlinelongest"))
+		{
+			if(sender instanceof Player && !sender.hasPermission("spacebugutils.onlinelongest"))
+				sender.sendMessage("You ain't got the perms foo!");
+			else
+			{
+				long curTime = System.currentTimeMillis();
+				for(String p : list)
+				{
+					Integer t = (int) (curTime - joined.get(p))/1000;
+					
+					String time = "";
+					if (t >= 3600)
+					{
+						Integer hours = t / 3600; //how many hours
+						Integer min = (t - (hours * 3600))/60;// left over minutes
+						time = hours.toString() + " hour(s) " + min.toString() + " minutes";
+					}
+					else if (t >= 60)
+					{
+						Integer min = t/60;
+						time = min.toString() + " minutes";
+					}
+					else
+						time = t.toString() + " seconds";
+					sender.sendMessage(ChatColor.GREEN+p+ " " + time);
+				}
+			}
+		}
+		
 		//silentTP
-		if(cmd.getName().equalsIgnoreCase("silenttp"))
+		else if(cmd.getName().equalsIgnoreCase("silenttp"))
 		{
 			if(!(sender instanceof Player))
 				sender.sendMessage("This command can only be run by a player.");
 			else
 			{
 				Player p = (Player) sender;
+				if(!p.hasPermission("spacebugutils.silenttp")) return true;
 				switch (args.length)
 				{
 				case 1: //proper amount of players sent
@@ -64,6 +140,19 @@ public class Main extends JavaPlugin implements Listener
 				}
 			}
 		}
+		
+		//sbureload
+		else if(cmd.getName().equalsIgnoreCase("sbureload"))
+		{
+			//if is player, and doesn't have permission
+			if((sender instanceof Player) && !(sender.hasPermission("spacebugutils.reload")))
+					sender.sendMessage(ChatColor.DARK_RED+"No permission.");
+			else
+			{
+				FileManager.reloadPlayers();
+				//loadConfig();
+			}
+		}
 		return true;
 	}
 
@@ -80,26 +169,119 @@ public class Main extends JavaPlugin implements Listener
 				if(event.getClickedBlock().getType() == Material.MOB_SPAWNER)
 					event.setCancelled(true);
 	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onEntityDeath(EntityDeathEvent event)
+	{
+		//only care about dragons
+		if(event.getEntityType() != EntityType.ENDER_DRAGON) return;
+		//make sure this is the same dragon that just got attacked. Should never not be the case, but whatevs
+		if(!dragon.equals(event.getEntity())) return;
+		
+		//get the last attacker of the dragon (the one that caused the death)
+		Player slayer = lastAttacker;
+		
+		String id = slayer.getUniqueId().toString();
+		DateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		
+		FileManager.getPlayers().set(id+".name", slayer.getName());
+		FileManager.getPlayers().set(id+".on", dateformat.format(date));
+		FileManager.savePlayers();
+		
+		slayer.sendMessage(ChatColor.GOLD+"Congratulations on slaying the dragon!");
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onHarm(EntityDamageByEntityEvent event)
+	{
+		//only care about dragons
+		if(event.getEntityType() != EntityType.ENDER_DRAGON) return;
+		dragon = event.getEntity();
+		Player slayer = null;
+		
+		//check if the player is damaging directly
+		if(event.getDamager() instanceof Player)
+			slayer = (Player)event.getDamager();
+		
+		//check if it was an arrow fired by a player
+		else if (event.getDamager() instanceof Arrow)
+		{
+			Arrow arrow = (Arrow)event.getDamager();
+			if(arrow.getShooter() instanceof Player)
+				slayer = (Player)arrow.getShooter();
+		}
+		
+		//wasn't a player who damaged the dragon
+		if(slayer == null) return;
+		
+		//see if they've killed the dragon before
+		String id = slayer.getUniqueId().toString();
+		lastAttacker = slayer;
+		if(FileManager.getPlayers().contains(id))
+		{
+			slayer.sendMessage(ChatColor.GOLD+"Stay your weapon, slayer. Allow someone else to kill the beast!");
+			event.setDamage(0);; //hopefully this will prevent mcMMO from interferring.
+			event.setCancelled(true);
+		}
+	}
   
-	//prevent players from going above the nether
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onCraft(PrepareItemCraftEvent event)
+	{
+		//don't care about anything other than banners right now
+		if(event.getRecipe().getResult().getType() != Material.BANNER) return;
+		int banners = 0; //track how many banners are being used to craft
+		for(ItemStack i : event.getInventory().getMatrix())
+			if(i != null && i.getType() == Material.BANNER)
+				banners++;
+		//0 == crafting blank banner; 2 == copying banner
+		if(!(banners == 0 || banners == 2))
+		{
+			event.getInventory().setResult(new ItemStack(Material.AIR)); //set the result to air
+			for(HumanEntity p : event.getViewers())
+				if(p instanceof Player)
+					p.sendMessage(ChatColor.RED + "Special banners can not be crafted, you must earn them!");
+		}
+	}
+	
 	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onPlayerMove(PlayerMoveEvent event)
 	{
 		Player p = event.getPlayer();
 		if (p.hasPermission("spacebugutils.nether")) return;
-		if (p.getLocation().getWorld().getEnvironment().equals(World.Environment.NETHER))
+		Location to = event.getTo();
+		if (to.getWorld().getEnvironment().equals(World.Environment.NETHER))
 		{
-			if (p.getLocation().getBlockY() > 127)
+			if (to.getBlockY() > 127)
 			{
 				p.performCommand("spawn");
-				p.performCommand("mail send 10becja AUTO above the nether " + p.getLocation().getBlockX() + " " + p.getLocation().getBlockZ());
-				p.sendMessage(ChatColor.RED + "Going above the Nether ceiling is NOT allowed");
+				p.sendMessage(ChatColor.RED + "Going above the Nether ceiling is not allowed");
 				this.logger.info("[SpaceBugUtils] "+ p.getName() + " was prevented from going above nether.");
+				event.setTo(event.getFrom());
+				event.setCancelled(true);
 			}
 		}
 	}
 	
-	//prevent players using too many caps, or spamming chat
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onTP(PlayerTeleportEvent event)
+	{
+		Player p = event.getPlayer();
+		if (p.hasPermission("spacebugutils.nether")) return;
+		Location to = event.getTo();
+		if (to.getWorld().getEnvironment().equals(World.Environment.NETHER))
+		{
+			if (to.getBlockY() > 127)
+			{
+				p.sendMessage(ChatColor.RED + "Going above the Nether ceiling is not allowed");
+				this.logger.info("[SpaceBugUtils] "+ p.getName() + " was prevented from going above nether.");
+				event.setTo(event.getFrom());
+				event.setCancelled(true);
+			}
+		}
+	}
+
 	@EventHandler(priority=EventPriority.LOWEST)
 	public void onChat(AsyncPlayerChatEvent event)
 	{
@@ -120,4 +302,10 @@ public class Main extends JavaPlugin implements Listener
 		if ((double) caps/low > 1 && msg.length() > 5)
 			event.setMessage(msg.toLowerCase());
 	}
+	
+	@SuppressWarnings("unused")
+	private void print(String p){System.out.println(p);}
+	public static JavaPlugin getInstance() {return plugin;}
 }
+
+
